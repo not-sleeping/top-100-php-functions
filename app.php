21c24ad33c5554988a624b3ff5b4a38ebc36bb33
Max de Hoog <maxdehoog@hotmail.com>
2022-12-26 19:03:34 +0100

    a workaround for ordering the functions

--- app.php
+++ app.php
@@ -1,12 +1,54 @@
 <?php
 
+// get all files we need
 $files = glob( __DIR__ . '/php/*.php');
 
+/**
+ *  the files are named with strings
+ *  we need to do something to prevent the way its ordered by PHP
+ *  strings in an ascending order will look like this:
+ *  1 
+ *  10
+ *  2
+ *  3
+ *  4
+ */
+
+$fileOrder = [];
+$fileNames = [];
+
 foreach ($files as $file) {
-    $name = str_replace('-', ' - ', basename($file, ".php"));
-    echo '<h3 style="margin:0px; padding:0px;">'.$name.'()</h3>';
-    require($file);
-    echo '</br></br>';
+    
+    // get the filename
+    $name = basename($file, '.php');
+    // get the number out of the filename
+    $order = strtok($name, '-');
+    // push the number into the array
+    array_push($fileOrder, $order);
+
+    // get the position of the dash
+    $dash = strpos($name, '-');
+    // remove everything behind the dash, including the dash
+    $prettyname = substr($name, $dash + 1);
+    // push the name into a seperate array
+    array_push($fileNames, $prettyname);
+};
+
+// combine the arrays so the number with be the key for each file
+$allFiles = array_combine($fileOrder, $fileNames);
+
+// sort the array based on the key
+ksort($allFiles);
+
+for($i = 1; $i <= count($allFiles); $i++)
+{
+    // reconstruct path
+    $filename = $i.'-'.$allFiles[$i];
+    $path = __DIR__ . '/php/'.$filename.'.php';
+
+    // show name and output
+    echo '<h3 style=""padding:0px; margin:0px;>'.$i.' '.$allFiles[$i].'()</h3>';
+    require($path);
 }
 
 ?>
---
+++ php/10-strpos.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * strpos(string, string, integer)
+ * string: a piece of text
+ * string: string you are looking for in the text
+ * integer: offset in characters from index 0
+ */
+
+ $inputStrpos = 'where is this position';
+
+ $output = strpos($inputStrpos, 'e', 3);
+
+ var_dump($output);
+
+ // returns 4
+
+?>
