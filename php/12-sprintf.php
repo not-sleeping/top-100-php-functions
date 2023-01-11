d86e6984aaac16a90a2c35fc25a2b41fe9b5c709
Max de Hoog <maxdehoog@hotmail.com>
2023-01-11 18:45:49 +0100

    added description to sprintf and simplified the function itself

--- php/12-sprintf.php
+++ php/12-sprintf.php
@@ -4,9 +4,13 @@
  * sprintf(string, ...values): string
  * string: piece of text you would like to format
  * ...values: comma seperated values used for formatting
+ * 
+ * Each value added to the list can be accessed by a numeric list, unlike an array it starts at 1
+ * For example: %s becomes %1$s, %1 let's the function know you want the first value in the comma separated list
+ * There are a lot of values (flags & specifiers) that can be used, for more information look at: https://www.php.net/manual/en/function.sprintf.php
  */
 
- $text = 'I am %2$.0f years old and I\'m living in %1$\'*10s';
+ $text = 'I am %2$f years old and I\'m living in %1$s';
  $age = 22.145;
  $location = 'Rotterdam';
 
