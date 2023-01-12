6c332972fd268733f0a46b00b039deb9fa176f77
Max de Hoog <maxdehoog@hotmail.com>
2023-01-12 14:35:24 +0100

    added is_string function

---
+++ php/16-is_string.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * is_string(mixed): boolean
+ * string: check if the value is a string
+ */
+
+ $text = 'Is this a string?';
+
+ $output = is_string($text);
+
+ var_dump($output);
+
+ // returns 1|true
+
+?>
