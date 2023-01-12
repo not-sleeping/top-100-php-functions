180df5ec717d259a21c8fac0feee7cae4157a88e
Max de Hoog <maxdehoog@hotmail.com>
2023-01-12 14:28:17 +0100

    added strtolower function

---
+++ php/14-strtolower.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * strtolower(string): string
+ * string: a piece of text you want to have all lowercase
+ */
+
+ $text = 'I WanT ThIs To LoWERcaSe 123';
+
+ $output = strtolower($text);
+
+ var_dump($output);
+
+ // returns 'i want this to lowercase 123'
+
+?>
